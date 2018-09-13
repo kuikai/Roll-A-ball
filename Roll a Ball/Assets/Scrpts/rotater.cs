78ead4c5e464435a235506b6a5cc6e29af11d510
kuikai <mikkeldixen@live.dk>
2018-09-13 16:11:02 +0200

    dd

---
+++ Roll a Ball/Assets/Scrpts/rotater.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotater : MonoBehaviour {
+
+    float smooth = 5.0f;
+    bool rotate = true;
+	// Update is called once per frame
+	void Update () {
+
+        while (rotate==true)
+        {
+            Quaternion Taget = Quaternion.Euler(1, 1, 1);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Taget, Time.deltaTime * smooth);
+        }
+        }
+}
