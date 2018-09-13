e349be3f4cd78586d9f54bc081b19662e68296d0
kuikai <mikkeldixen@live.dk>
2018-09-13 13:00:25 +0200

    2push

---
+++ Roll a Ball/Assets/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    Rigidbody rb;
+	void Start () {
+      
+
+
+     
+
+        GetComponent<Rigidbody>();
+		
+	}
+
+	
+	
+    // Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Roll a Ball/Assets/Scrpts/Movee.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movee : MonoBehaviour {
+    public float speed;
+    Rigidbody rb;
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        
+	}
+
+    // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        float movH = Input.GetAxisRaw("Horizontal");
+        float movV = Input.GetAxisRaw("Vertical");
+
+
+
+        Vector3 movement = new Vector3(movH, 0.0f, movV);
+        rb.AddForce(movement * speed);
+
+        
+    }
+    void Update () {
+		
+
+
+
+	}
+}
